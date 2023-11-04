0c89997a49bd67332cc1d18ca4f7accaca3eb309
Bruno Beghelli <bruno.beghelli@gmail.com>
2023-11-04 16:23:46 -0300

    Sorts conferences by date and city by default, fills automatically created at comment date

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Comment
 {
     #[ORM\Id]
@@ -109,4 +110,10 @@ class Comment
 
         return $this;
     }
+
+	#[ORM\PrePersist]
+	public function setCreatedAtValue()
+	{
+		$this->createdAt = new \DateTimeImmutable();
+	}
 }
